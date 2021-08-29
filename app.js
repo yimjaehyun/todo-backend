1ccd4a5e34705e11d6b47ab8bf2d7cfc7b5ca9ea
Jae <jaehyunyim1997@gmail.com>
2021-08-29 13:38:12 -0700

    Add add/item endpoint

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+
 const app = express()
+
+// parse application/json
+app.use(bodyParser.json())
+
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+class todoItem {
+	constructor(description, deadline, attachment) {
+		this.description = description
+		this.deadline = deadline
+		this.attachment = attachment
+	}
+}
+
+var DB = {}
+
+app.get('/items', (req, res) => {
+
+})
+
+app.get('/items/:id', (req, res) => {
+
+})
+
+app.post('/add/item', (req, res) => {
+	console.log(req.body)
+	// Required fields verification
+	const description = req.body.description
+	const deadline = req.body.deadline
+	const attachment = req.body.attachment
+
+	if (!description || !deadline || !attachment) {
+		res.status(400).send("Description, Deadline, and Attachment fields are required.")
+	}
+
+	// Add to db, using local storage for simplicity
+	var itemKey = Object.keys(DB).length // use length as key
+	var itemObj = new todoItem(description, deadline, attachment)
+	DB[itemKey] = itemObj
+	// Since using local storage, always send success
+	res.status(200).send({[itemKey]: itemObj})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`Example app listening at http://localhost:${port}`)
 })
