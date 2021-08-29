43a30ca5273ae7ed8de1e86c617ede7a7367740a
Jae <jaehyunyim1997@gmail.com>
2021-08-29 14:16:40 -0700

    Handle attachments

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const upload = require('express-fileupload')
 var bodyParser = require('body-parser')
 
 const app = express()
 
 // parse application/json
 app.use(bodyParser.json())
+app.use(upload())
 
 const port = 3000
 
@@ -29,15 +31,24 @@ app.post('/add/item', (req, res) => {
 	// Required fields verification
 	const description = req.body.description
 	const deadline = req.body.deadline
-	const attachment = req.body.attachment
+	const file = req.files.file
 
-	if (!description || !deadline || !attachment) {
-		res.status(400).send("Description, Deadline, and Attachment fields are required.")
+	// Add attachment to DB, using local storage for simplicity
+	file.mv('./attachments/'+file.name, function(err) {
+		if (err) {
+			res.status(400).send(err)
+		} else {
+			console.log("File uploaded successfully")
+		}
+	})
+
+	if (!description || !deadline) {
+		res.status(400).send("Description and Deadline fields are required.")
 	}
 
 	// Add to db, using local storage for simplicity
 	var itemKey = Object.keys(DB).length // use length as key
-	var itemObj = new todoItem(description, deadline, attachment)
+	var itemObj = new todoItem(description, deadline, file)
 	DB[itemKey] = itemObj
 	// Since using local storage, always send success
 	res.status(200).send({[itemKey]: itemObj})
