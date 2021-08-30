e09152214ef0da790c8e483b778b9f07283c967b
Jae <jaehyunyim1997@gmail.com>
2021-08-29 19:08:20 -0700

    Add Dismissed

--- app.js
+++ app.js
@@ -4,18 +4,25 @@ var bodyParser = require('body-parser')
 
 const app = express()
 
+
+// Debugging on localhost
+const cors = require("cors")
+app.use(cors())
+
 // parse application/json
 app.use(bodyParser.json())
 app.use(upload())
 
-const port = 3000
+const port = 8000
 
 class todoItem {
-	constructor(description, deadline, attachment) {
+	constructor(id, description, deadline, attachment) {
+		this.id = id
 		this.description = description
 		this.deadline = deadline
 		this.attachment = attachment
 		this.isComplete = false
+		this.isPassed = CompareDeadline(deadline)
 	}
 }
 
@@ -25,60 +32,70 @@ function isIsoDate(str) {
   return d.toISOString()===str;
 }
 
+function CompareDeadline(date) {
+	if (date.setHours(0, 0, 0, 0) <= (new Date()).setHours(0, 0, 0, 0)) {
+		return true;
+	}
+	return false;
+}
+
 var DB = {}
 
 // Get all items in our TODO
 app.get('/items', (req, res) => {
-	res.send(DB)
+	res.send(Object.values(DB))
 })
 
 // Add new TODO item
 app.post('/add/item', (req, res) => {
 	// Required fields verification
+	const id = req.body.id
 	const description = req.body.description
 	var deadline = req.body.deadline
-	const file = req.files.file
+	var file = null
 
-	if (!description || !deadline) {
-		res.status(400).send("Description and Deadline fields are required.")
+	if (!id || !description || !deadline) {
+		return res.status(400).send("Id, Description, and Deadline fields are required.")
 	}
-
-	// Add attachment to DB, using local storage for simplicity
-	file.mv('./attachments/'+file.name, function(err) {
-		if (err) {
-			res.status(400).send(err)
-		} else {
-			console.log("File uploaded successfully")
-		}
-	})
 
 	// Verify Date is in correct format and parse it
 	if (!isIsoDate(deadline)) {
-		res.status(400).send("Please use valid UTC Date format: YYYY-MM-DDTHH:MN:SS.MSSZ")
+		return res.status(400).send("Please use valid UTC Date format: YYYY-MM-DDTHH:MN:SS.MSSZ")
 	}
 
 	deadline = new Date(deadline)
 
+	// Add attachment to DB, using local storage for simplicity
+	if (req.files) {
+		file = req.files.file
+		file.mv('./attachments/'+file.name, function(err) {
+			if (err) {
+				return res.status(400).send(err)
+			} else {
+				console.log("File uploaded successfully")
+			}
+		})
+	}
+
 	// Add to db, using local storage for simplicity
-	var itemKey = Object.keys(DB).length // use length as key
-	var itemObj = new todoItem(description, deadline, file)
-	DB[itemKey] = itemObj
+	var itemObj = new todoItem(id, description, deadline, file)
+	DB[id] = itemObj
 	// Since using local storage, always send success
-	res.status(200).send({[itemKey]: itemObj})
+	return res.status(200).send({[id]: itemObj})
 })
 
 // Make todo item as complete
-app.post('/complete/item/:itemId', (req, res) => {
+app.patch('/complete/item/:itemId', (req, res) => {
 	// Required fields verification
 	const itemId = req.params.itemId
 	const isComplete = req.body.isComplete
 
-	if (!itemId || !isComplete) {
-		res.status(400).send("itemId and isComplete fields are required.")
+	if (!itemId || isComplete == null) {
+		return res.status(400).send("itemId and isComplete fields are required.")
 	}
 
 	DB[itemId].isComplete = isComplete
-	res.status(200).send({[itemId] : DB[itemId]})
+	return res.status(200).send({[itemId] : DB[itemId]})
 })
 
 app.listen(port, () => {
